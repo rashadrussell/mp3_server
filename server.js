4232e1222f29f7d215d299467c27d069f1e518b9
Rashad Russell <rjrusse2@illinois.edu>
2015-04-12 04:03:58 +0000

    save

--- server.js
+++ server.js
@@ -49,11 +49,11 @@ usersRoute.get(function(req, res) {
 	var query = User.find('name email id', function(err, users) {
 
 		if(err) {
-			res.json({
+			res.status(500).json({
 				"message": "Internal Server Error: " + err,
 				"data": {}
 			});
-			res.status(500);
+			
 			return;
 		}
 
@@ -85,25 +85,25 @@ usersRoute.get(function(req, res) {
 
 	query.exec(function(err, users) {
 		if(err) {
-			res.json({
+			res.status(500).json({
 				"message": "Server Error",
 				"data": {}
 			});
-			res.status(500);
+			
 
 		} else {
 			if(!users) {
-				res.json({
+				res.status(404).json({
 					"message": "Users Not Found",
 					"data": {}
 				});
-				res.status(404);
+				
 			} else {
-				res.json({
+				res.status(200).json({
 					"message": "OK",
 					"data": users
 				});
-				res.status(200);
+				
 			}
 
 		}
@@ -117,31 +117,31 @@ usersRoute.get(function(req, res) {
 		newUser;
 
 	if(!name || !email) {
-		res.json({
+		res.status(400).json({
 			"message": "Name or email not included",
 			"data": {}
 		});
-		res.status(400);
+		
 		return;
 	}
 
 	User.count({"email": email}, function(err, count) {
 
 		if(err) {
-			res.json({
+			res.status(500).json({
 				"message": "Internal Server Error: " + err,
 				"data": {}
 			});
-			res.status(500);
+			
 			return;
 		}
 
 		if(count > 0) {
-			res.json({
+			res.status(400).json({
 				"message": "User with this email already exists",
 				"data": {}
 			});
-			res.status(400);
+			
 			return;
 		}		
 
@@ -156,11 +156,11 @@ usersRoute.get(function(req, res) {
 
 	console.log(newUser);
 	newUser.save();
-	res.json({
+	res.status(201).json({
 		"message": "OK",
 		"data": newUser
 	});
-	res.status(200);
+	
 })
 .options(function(req, res){
       res.writeHead(200);
@@ -172,30 +172,34 @@ var userRoute = router.route('/users/:id');
 
 userRoute.get(function(req, res) {
 
-	User.findById(req.params.id, 'name email pendingTasks', function(err, user) {
+	User.findById(req.params.id, function(err, user) {
 		if(err) {
-			res.json({
+			res.status(500).json({
 				"message": "Internal Server Error: " + err,
 				"data": {}
 			});
-			res.status(500);
+			
 			return;
 		}
 
 		if(!user) {
-			res.json({
+			
+			res.status(404).json({
 				"message": "Not Found - User with ID: " + req.params.id,
 				"data": {}
 			});
-			res.status(404);
-			return;
+
+		} else {
+			res.status(200).json({
+				"message": "OK",
+				"data": user
+			});
+			
+
 		}
 
-		res.json({
-			"message": "OK",
-			"data": user
-		});
-		res.status(200);
+		
+		
 	});
 })
 .delete(function(req, res) {
@@ -203,29 +207,29 @@ userRoute.get(function(req, res) {
 	var query = User.find({_id: req.params.id}, function(err, user) {
 		if(err) {
 			console.error(err);
-			res.json({
+			res.status(500).json({
 				"message": "Error: " + err,
 				"data": {}
 			});
-			res.status(500);
+			
 			return;
 		}
 
 		if(!user[0]) {
-			res.json({
+			res.status(404).json({
 				"message": "User Not Found",
 				"data": {}
 			});
-			res.status(404);
+			
 			return;
 
 		} else {
 			user[0].remove();
-			res.json({
+			res.status(200).json({
 				"message": "OK",
 				"data": {}
 			});
-			res.status(200);
+			
 			return;
 		}
 
@@ -240,11 +244,10 @@ tasksRoute.get(function(req, res) {
 	
 	var query = Task.find(function(err, tasks) {
 		if(err) {
-			res.json({
+			res.status(500).json({
 				"message": "Internal Server Error: " + err,
 				"data": {}
 			});
-			res.status(500);
 			return;
 		}
 	});
@@ -275,25 +278,25 @@ tasksRoute.get(function(req, res) {
 
 	query.exec(function(err, tasks) {
 		if(err) {
-			res.json({
+			res.status(500).json({
 				"message": "Server Error",
 				"data": {}
 			});
-			res.status(500);
+			
 
 		} else {
 			if(!tasks) {
-				res.json({
+				res.status(404).json({
 					"message": "Tasks Not Found",
 					"data": {}
 				});
-				res.status(404);
+				
 			} else {
-				res.json({
+				res.status(200).json({
 					"message": "OK",
 					"data": tasks
 				});
-				res.status(200);
+				
 			}
 
 		}
@@ -327,11 +330,11 @@ tasksRoute.get(function(req, res) {
 	}
 
 	newTask.save();
-	res.json({
+	res.status(201).json({
 		"message": "OK",
 		"data": newTask
 	});
-	res.status(200);
+	
 })
 .options(function(req, res){
       res.writeHead(200);
@@ -344,27 +347,27 @@ var taskRoute = router.route('/tasks/:id');
 taskRoute.get(function(req, res) {
 	Task.findById(req.params.id, function(err, task) {
 		if(err) {
-			res.json({
+			res.status(500).json({
 				"message": "Internal Server Error: " + err,
 				"data": {}
 			});
-			res.status(500);
+			
 			return;
 		}
 
 		if(!task) {
-			res.json({
+			res.status(404).json({
 				"message": "Not Found - Task with ID: " + req.params.id,
 				"data": {}
 			});
-			res.status(404);
+			
 			return;
 		}
-		res.json({
+		res.status(200).json({
 			"message": "OK",
 			"data": task
 		});
-		res.status(200);
+		
 	});
 })
 .put(function(req, res) {
@@ -380,6 +383,23 @@ taskRoute.get(function(req, res) {
 
     
 	Task.findById(req.params.id, function(err, task) {
+
+
+		if(err) {
+			res.status(500).json({
+				"message": "Error: " + err,
+				"data": {}
+			});
+			return;
+		}
+
+		if(!task) {
+			res.status(404).json({
+				"message": "Cannot Find Task",
+				"data": {}
+			});
+			return;
+		}
 
 		if(task.assignedUser !== "" && (editedUser.assignedUser !== task.assignedUser)) {
 
@@ -415,36 +435,41 @@ taskRoute.get(function(req, res) {
 		
 	});
 
+	res.status(201).json({
+		"message": "Task Updated",
+		"data": {}
+	});
+
 })
 .delete(function(req, res) {
 
 	Task.find({_id: req.params.id}, function(err, task) {
 
 		if(err) {
-			res.json({
+			res.status(500).json({
 				"message": "Error: " + err,
 				"data": {}
 			});
-			res.status(500);
+			
 			return;
 
 		} 
 
 		if(!task[0]) {
-			res.json({
+			res.status(404).json({
 				"message": "Task Not Found",
 				"data": {}
 			});
-			res.status(404);
+			
 			return;
 
 		} else {
 			task[0].remove();
-			res.json({
+			res.status(200).json({
 				"message": "OK",
 				"data": {}
 			});
-			res.status(200);
+			
 		}
 
 	});
